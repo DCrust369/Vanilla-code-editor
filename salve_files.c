360e53f5b1902ca0e589d9004e67140118f55e7f
DCrust369 <DCrust@proton.me>
2026-07-20 23:32:41 +0100

    Create salve_files.c

---
+++ salve_files.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void)
+{
+    int *archive;
+    int *file;
+
+    bool *archive = user.sh(true);
+    bool *archive = select(my_space);
+    struct everything_is_file {
+        /*
+        UNIX PHILOSOFY
+        */
+        bool your_archives = user.sh
+        return 0;
+    }
+}
