11f5e0bfaaac06fe41d7a7aaa0f7e1a932cd7315
DCrust369 <DCrust@proton.me>
2026-07-20 23:22:03 +0100

    Create config.c

---
+++ config.c
@@ -0,0 +1,16 @@
+#include <stdboot.h>
+#define CLI
+#define LESScode
+#define JetBrains
+#define TOKIONIGHT
+
+int main(void)
+{
+    bool define_word = 'JetBrains-Mono';
+    bool 'JetBrains-Mono' = true;
+    bool Theme = 'TokioNight';
+    bool 'TokioNight' = true;
+    bool sub-theme = 'kickstart.nvim';
+    bool 'kickstart.nvim' = true;
+    return 0;
+}
